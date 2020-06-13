52b9f672d7f067b870b9397b3b55197d4f64200a
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-06-13 14:41:25 +0100

    unfixed code

--- journal.js
+++ journal.js
@@ -1,44 +1,47 @@
 console.log('it works');
 
-const journalEntries =
-    [
-        {
-            "title": "My favourite sport",
-            "content": "When have spare time, I always to play football as it my favourite sport."
-        },
+const journalEntries = {
+    "title":
+        ["My favourite sport",
+            "The song I like the most",
+            "My favourite day"
+        ],
 
-        {
-            "title": "The song I like the most",
-            "content": "My favourite song at the moment is voclist musics, especially it is with guitar."
-        },
+    "content":
+        ["When have spare time, I always to play football  as it my favourite sport.",
+            "My favourite song at the moment is voclist musics,    especially it is with guitar.",
+            "I'm always motivated every Monday as it is the first day of a week."
+        ]
 
-         {
-            "title": "My favourite day",
-            "content": "I'm always motivated every Monday as it is the first day of a week."
-        }
-    ];
+};
 
-const journalEntry = function () {
-    return journalEntries.map(function(entry) {
-         return entry.title + " \n " + entry.content;
-    })
-// for (let i = 0; i <= journalEntries.length; i++) {
-//     return journalEntries[i].title + '\n ' + journalEntries[i].content;
-// }
+
+function displayMenu() {
+    let entry = '';
+
+    while (entry === '') {
+        const entries = Number(prompt("Choose 1 to see the list of journal entries. \n Choose 2 to add a new journal entry \n Choose 3 to quit"));
+        if (entries === '1') {
+            alert(`${journalEntries.title[0]} \n ${journalEntries.content[0]}`);
+            alert(`${journalEntries.title[1]} \n ${journalEntries.content[1]}`);
+            alert(`${journalEntries.title[2]} \n ${journalEntries.content[2]}`);
+
+        } else if (entries === '2') {
+            let  newJournalEntries = prompt('Enter your new journal entry');
+            // var  newEntrie = prompt('Enter your new journal content');  
+            if (newJournalEntries === 'title') {
+                newJournalEntries = prompt('Enter your new journal title'); 
+            } else if (newJournalEntries === 'content') {
+                newEntrie = prompt('Enter your new journal content'); 
+            }
+        
+        }  
+        entry = "";
+
+        if (entries === '3') {
+            saveIt = true;
+            break;
+        }   
+    } 
 }
-journalEntry();
-
-const entries = Number(prompt("Choose 1 to see the list of journal entries. \n Choose 2 to add a new journal entry \n Choose 3 to quit"))
-if (entries === 1) {
-    alert(journalEntry());
-} else if (entries === 2) {
-     const newJournalEntry = prompt("Add a new journal entry");
-     const entry = [];
-     for (let {title, content} of anotherEntry) {
-         for (let newTitle of title) {
-             newJournalEntry = entry.push({newTitle, content});
-         }
-     }
-} else if (entries === 3) {
-
-// }
+displayMenu();
