9efb20f3a5f66afef5f66a56d8a5ce31cd0d6b3a
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-06-16 16:35:14 +0100

    added

--- log-in/log-in.js
+++ log-in/log-in.js
@@ -1,1 +1,34 @@
 console.log("Hello");
+
+const socialProgram = [
+    {
+      title: "Confidence",
+      link: '"http://confidence.com"',
+      author: "Jonshon"
+    },
+    
+    {
+      title: "Motivation",
+      link: '"https://www.motivation.com"',
+      author: "Ariane"
+    },
+    
+    {
+      title: "Diligence",
+      link: '"http://diligence.org"',
+      author: "Sonia"
+    }
+  ];
+  
+  console.log(socialProgram);
+  
+  const listStartMenu = `Choose an option:
+  1: Show links
+  2: Add a link
+  3: Remove link
+  0: Quit
+  `;
+  
+  
+  let startMenu = Number(prompt(listSartMenu));
+  console.log(startMenu);
