40e2f24f6d85f1c0be54a204657d817496cd5899
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-06-20 14:36:34 +0100

    I was doing this journal project again

--- journal.js
+++ journal.js
@@ -2,81 +2,73 @@ console.log('it works');
 
 // this is my main journal array
 const journal = [
-    {
-        title: "My favourite sport",
-        content: "When have spare time, I always to play football  as it my favourite sport."
-    },
+	{
+		title: "My favourite sport",
+		content: "When have spare time, I always to play football  as it my favourite sport."
+	},
 
-    {
-        title: "The song I like the most",
-        content: "My favourite song at the moment is voclist musics,    especially it is with guitar."
-    },
+	{
+		title: "The song I like the most",
+		content: "My favourite song at the moment is voclist musics,    especially it is with guitar."
+	},
 
-    {
-        title: "My favourite day",
-        content: "I'm always motivated every Monday as it is the first day of a week."
-    }
+	{
+		title: "My favourite day",
+		content: "I'm always motivated every Monday as it is the first day of a week."
+	}
 
 ];
 
 // this function will list all the entries from the journal
-const listEntries = () => {
+const listJournal = () => {
 	for (let i = 0; i < journal.length; i++) {
-		let entryString = `Title : ${journal[i].title}
-Content : ${journal[i].content}`;
-		alert(entryString);
+		let allEntries = `Title: ${journal[i].title} \n Content: ${journal[i].content}`;
+		alert(allEntries);
 	}
-};
+}
+
 // this function will add a new entry to the journal array
 const addNewEntry = () => {
-	const entryTitle = prompt('What is the title of your entry?');
-	const entryContent = prompt('What is the content?');
-	let entry = {
-		title: entryTitle,
-		content: entryContent,
+	let newTitle = prompt("Enter a new title of your journal");
+	let newContent = prompt('Write your content');
+
+	let newJournal = {
+		title: newTitle,
+		content: newContent
 	};
-	journal.push(entry);
-	return;
-};
-// this is my menu string, that we will pass to the prompt call
-const menuString = `Welcome to my Onja Journal! 📕
-Choose (1) for listing all the entries.
-Choose (2) for adding a new entry.
-Choose (3) to quit.
-Choose (4) to delete the last entry.
-Choose (5) to delete a specific entry. (with its index)
-`;
-let menuChoice = Number(prompt(menuString));
-// we run the program until the user enters 3 (the quit option)
-while (menuChoice !== 3) {
-	switch (menuChoice) {
-		case 1:
-			listEntries();
-			break;
-		case 2:
-			addNewEntry();
-			break;
-		case 4:
-			const entryDeleted = journal.pop();
-			alert(`We deleted the last entry : ${entryDeleted.title}`);
-			break;
-		case 5:
-			let indexToDelete = Number(
-				prompt(`Enter the entry's index to delete (1 to ${journal.length})`)
-			);
-			let deletedEntry = journal.splice(indexToDelete--, 1);
-			console.log(deletedEntry);
-			alert(`You just deleted ${deletedEntry[0].title}`);
-			break;
-		default:
-			alert('Please enter a correct choice. (1,2,3,4,5)');
-	}
-	menuChoice = Number(prompt(menuString));
+
+	journal.push(newJournal);
 }
+
+const deletingJournal = () => {
+	journal.splice(-1);
+}
+
+// this is my menu string, that we will pass to the prompt call
+let listOfChoice = ` Welcome to this journal
+Choose (1) to see the list of journal
+Choose (2) to add a new journal
+Choose (3) to quit
+Choose (4) to remove a journal
+`;
+let choice = Number(prompt(listOfChoice));
+
+//we run the program until the user enters 3 (the quit option)
+while (choice !== 3) {
+	if (choice === 1) {
+		listJournal();
+	}
+	else if (choice === 2) {
+		addNewEntry();
+	}
+	else if (choice === 4) {
+		deletingJournal();
+	}
+	else {
+		alert("Enter the right choice");
+	}
+	choice = Number(prompt(listOfChoice));
+}
+
 // here, this is the end of the program. It's the last instruction.
-alert('Thank you for using my program');
-
-
-
-
-
+alert("Thanks of seeing and adding journal entries");
